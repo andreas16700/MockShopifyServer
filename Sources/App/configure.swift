b28014e38f0358554d506a2346afc99a1cf5e40c
Andreas Loizides <andreas16700@icloud.com>
2023-03-23 03:14:10 +0200

    change port to 8082

--- Sources/App/configure.swift
+++ Sources/App/configure.swift
@@ -6,6 +6,6 @@ public func configure(_ app: Application) throws {
     // app.middleware.use(FileMiddleware(publicDirectory: app.directory.publicDirectory))
 	app.http.server.configuration.port = 8082
     // register routes
-	app.routes.defaultMaxBodySize = "500gb"
+	app.routes.defaultMaxBodySize = "999gb"
     try routes(app)
 }
